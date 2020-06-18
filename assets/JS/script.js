a8ebca1aee6ab78bf37ba3af9978e421d798776e
Kennadee <kennadee.ray@gmail.com>
2020-06-18 12:18:57 -0500

    add user selctors and cases to generate characters to choose from

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,46 @@
 // Assignment code here
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var number = "1234567890";
+var special = "!@#$%&*"
+
+var uCase = window.confirm("Do you want Uppercase?");
+var lCase = window.confirm("Do you want lowercase?");
+var num = window.confirm("Do youy want numbers?");
+var specChar = window.confirm("Do you want special characters?");
+
+var charList ="";
+
+if (uCase) {
+    charList = charList + upCase;
+}
+
+if (lCase) {
+  charList = charList +lowCase;
+}
+
+if (num) {
+  charList = charList + number;
+}
+
+if (specChar) {
+  charList = charList + special;
+}
+
+console.log(charList);
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
